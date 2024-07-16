aa56f3c595dc711cb39e582a9308c79b10972782
Saravana <100523948+Saravana-Pon-Balan@users.noreply.github.com>
2024-07-16 20:44:23 +0530

    Contact updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,28 +1,39 @@
+import React from 'react';
 
 function Contact() {
     return (
-        <div id="contact">
-            <section className="bg-white dark:bg-gray-900">
-                <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
-                    <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Need details about our Business plan? Let us know.</p>
-                    <form action="#" className="space-y-8">
-                        <div>
-                            <label htmlFor="email" className="block mb-2 text-sm font-semibold text-gray-900 dark:text-gray-300">Your email</label>
-                            <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@digitalemoji.com" required />
-                        </div>
-                        <div>
-                            <label htmlFor="subject" className="block mb-2 text-sm font-semibold text-gray-900 dark:text-gray-300">Subject</label>
-                            <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required />
-                        </div>
-                        <div className="sm:col-span-2">
-                            <label htmlFor="message" className="block mb-2 text-sm font-semibold text-gray-900 dark:text-gray-400">Your message</label>
-                            <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
-                        </div>
-                        <button type="submit" className="py-3 px-5 text-sm font-semibold text-center shadow-lg active:shadow-sm text-white rounded-lg bg-title-left sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
-                    </form>
+        <div id="contact" className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+            <div className="intro text-center mb-16">
+            <div className='title mb-10'>
+                <span className="text-title-left font-bold text-3xl">Contact</span>
+                <span className="text-title-right font-bold text-3xl "> Us</span>
+            </div>
+            <h2 className="text-3xl font-bold mb-2 text-blue-600">Hey There!</h2>
+            <p className="text-lg text-gray-700">We’re more than happy to have a conversation.</p>
+            </div>
+            <div className="cards grid grid-cols-1 md:grid-cols-3 gap-4">
+                {Array(3).fill(null).map((_, index) => (
+                    <div key={index} className="rounded-lg bg-card  h-80 w-72 cursor-pointer shadow-lg rounded-lg p-6 text-center">
+                        <h3 className="text-lg font-light mb-10">Project Enquiry</h3>
+                        <p className="mb-6 text-2xl font-bold text-center">Let us help you realise your vision.</p>
+                        <a href="#" class="group text-link transition duration-300">
+                        Let’s Talk
+                        <span class="block ml-20 mr-20 max-w-2 group-hover:max-w-full transition-all duration-500 h-1 bg-hover-link"></span>
+                        </a>
+
+                    </div>
+                ))}
+            </div>
+            <div className="text-center">
+                <h3 className="text-xl mt-16 font-semibold mb-4">Follow our work</h3>
+                <div className="flex justify-center space-x-6">
+                    <a href="#" className="text-link hover:text-hover-link">LinkedIn</a>
+                    <a href="#" className="text-link hover:text-hover-link">Instagram</a>
+                    <a href="#" className="text-link hover:text-hover-link">Facebook</a>
+                    <a href="#" className="text-link hover:text-hover-link">Dribbble</a>
+                    <a href="#" className="text-link hover:text-hover-link">Twitter</a>
                 </div>
-            </section>
+            </div>
         </div>
     );
 }
