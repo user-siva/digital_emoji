f428a53d882a4c112beb9a0f4ace4660fa2cf632
saravana <sravanafrancis16@gmail.com>
2024-07-25 21:43:07 +0530

    testimonial updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -42,19 +42,6 @@ function Hero() {
                     <div><b>30+</b> <br /> Global Awards</div>
                 </div>
             </section>
-
-            <section>
-                <figure className="flex flex-col items-center p-5">
-                    <Image src="/main.png" height={1400} width={1200} className="m-5" alt="Digital Marketing" />
-                    <figcaption className="max-w-screen-lg">
-                        <b>Digital Emoji Creation PVT/LTD</b> is full of bubbling ideas to sizzle your marketing
-                        communication. We are
-                        that digital agency which adds a glint to your business. Logo revamping, designing to marketing kit
-                        designing, website developing to 3D animation; you name it, we do it.
-                    </figcaption>
-                </figure>
-            </section>
-
             <div className="flex justify-center mt-8">
                 <button className="px-4 py-2 w-fit bg-blue-500 font-bold text-white rounded hover:bg-blue-700">
                     Talk To Our Architect
@@ -69,6 +56,41 @@ function Hero() {
                     </div>
                 ))}
             </div>
+            <section>
+                <figure className="flex flex-col items-center p-5">
+                    <Image src="/main.png" height={1400} width={1200} className="m-5" alt="Digital Marketing" />
+                    <figcaption className="max-w-screen-lg">
+                        <b>Digital Emoji Creation PVT/LTD</b> is full of bubbling ideas to sizzle your marketing
+                        communication. We are
+                        that digital agency which adds a glint to your business. Logo revamping, designing to marketing kit
+                        designing, website developing to 3D animation; you name it, we do it.
+                    </figcaption>
+                </figure>
+            </section>
+            <h1 className="font-bold mb-1 not-italic text-5xl mb-16 mt-16">
+                    Testimonials
+            </h1>
+            <section className="bg-white dark:bg-gray-900 rounded">
+                
+                <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
+                    <figure className="max-w-screen-md mx-auto">
+                        <svg className="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor" />
+                        </svg>
+                        <blockquote>
+                            <p className="text-2xl font-medium text-gray-900 dark:text-white">"Digital Emoji creation is just awesome. It contains tons of predesigned components and pages starting from login screen to complex dashboard. Perfect choice for your next SaaS application."</p>
+                        </blockquote>
+                        <figcaption className="flex items-center justify-center mt-6 space-x-3">
+                            <Image className="w-6 h-6 rounded-full" src="/photo1.jpeg" alt="profile picture" width={24} height={24} />
+                            <div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                                <div className="pr-3 font-medium text-gray-900 dark:text-white">Micheal Gough</div>
+                                <div className="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">CEO at Google</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </div>
+            </section>
+            
         </div>
     );
 }
